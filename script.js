f4a4beabe0f5ec10276505f15b53b4ea310b7cce
mocha <79212970+mochawoof@users.noreply.github.com>
2024-04-22 19:45:22 -0400

    Update script.js

--- script.js
+++ script.js
@@ -13,19 +13,6 @@ function makeElement(type, innerHTML, parent) {
   parent.appendChild(e);
 }
 
-//https://gist.github.com/PiJoules/7d78a95d53177468d479
-function dynamicSort(property) {
-    var sortOrder = 1;
-    if(property[0] === "-") {
-        sortOrder = -1;
-        property = property.substr(1);
-    }
-    return function (a,b) {
-        var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
-        return result * sortOrder;
-    }
-};
-
 function f(e) {
   if (e == null) {
     return "N/A";
