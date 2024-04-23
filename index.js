55a343032e71c7aca17705e2df758437a7ac945f
mocha <79212970+mochawoof@users.noreply.github.com>
2024-04-22 20:21:02 -0400

    documentation

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let repos = document.getElementById("repos");
     if (r.status == 200) {
       r.text().then((t) => {
         let allRepos = JSON.parse(t);
+        //sort repos
         allRepos.sort((a, b) => {
           if (a.updated_at != null && b.updated_at != null) {
             if (new Date(a.updated_at).getTime() > new Date(b.updated_at).getTime()) {
@@ -15,17 +16,22 @@ let repos = document.getElementById("repos");
             return 0;
           }
         });
+        //for each repo
         allRepos.forEach((e) => {
+          //get github url
           let url = e.html_url;
+          //set it to its homepage if it has one
           if (e.homepage != null && e.homepage != "") {
             url = e.homepage;
           } else if (e.has_pages) {
             url = "//" + username + ".github.io/" + e.name;
           }
+          //get license
           let license = "N/A";
           if (e.license != null) {
             license = e.license.spdx_id;
           }
+          //element title and style (for coloring)
           let title = e.full_name;
           let style = "";
           if (e.archived) {
